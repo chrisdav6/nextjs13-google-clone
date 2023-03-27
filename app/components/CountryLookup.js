dd39469346e7b28858ced8813d2182bad1a1db3d
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-03-27 15:02:14 -0400

    Created Footer component

---
+++ app/components/CountryLookup.js
@@ -0,0 +1,11 @@
+const CountryLookup = async () => {
+  // const res = await fetch(
+  //   `http://api.ipstack.com/134.201.250.155?access_key=${process.env.LOCATION_API}`
+  // );
+  // const data = await res.json();
+
+  // return <div>{data.country_name}</div>;
+  return <div>United States</div>;
+};
+
+export default CountryLookup;
