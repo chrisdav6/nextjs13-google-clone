5a99a94c597986d3e50a207b7f07a08291f73610
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2023-03-30 10:52:09 -0400

    Fetching sample data from the Google Search API

--- app/search/web/page.js
+++ app/search/web/page.js
@@ -1,5 +1,16 @@
-const WebSearch = () => {
-  return <div>WebSearch</div>;
+const WebSearch = async () => {
+  const res = await fetch(
+    `https://customsearch.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API}&cx=${process.env.GOOGLE_CX_KEY}&q=cars`
+  );
+  const data = await res.json();
+  const searchData = data.items;
+
+  return (
+    <div>
+      {searchData &&
+        searchData.map((item) => <p key={item.title}>{item.title}</p>)}
+    </div>
+  );
 };
 
 export default WebSearch;
